fe3519f4500ca9ffba4b65fc1306a81faa4e1ed0
rasberryrabbit <lovelust@hanmail.net>
2019-03-25 11:23:05 +0900

    Reduce start delay.

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 -- init.lua
 --Access Point management
 -- this code from http://www.microdev.it/wp/en/2016/11/28/nodemcu-enduser_setup-module-lua-samplepart2/
-pin = 5 --Input PIN
+pin = 5 --Reset PIN
 Resetta=0   --Variable used to manage AP reset
 gpio.mode(pin,gpio.INPUT)   --Pin 5 in input mode
 
@@ -31,8 +31,9 @@ else
         end
       else
         tmr.stop(0)
-        -- Start the webserver wiht ip defined by Wi-Fi router
-        tmr.alarm(0, 5000, 0, function() dofile("webserver.lua") end)
+        tmr.alarm(0, 3000, tmr.ALARM_SINGLE, function()
+          dofile("webserver.lua")
+        end)
       end
     end)
 
